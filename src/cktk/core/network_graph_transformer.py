2dcbd224d5fda6217dd98b02f6f6d1b3b4ef0060
Sean McLeaish <smcleaish@gmail.com>
2025-08-04 11:29:20 -0500

    Updating errors

--- src/cktk/core/network_graph_transformer.py
+++ src/cktk/core/network_graph_transformer.py
@@ -4,14 +4,24 @@ Network graph structure is defined in the NetworkGraphConfig object.
 """
 
 from dataclasses import dataclass
+import logging
 
 import polars as pl
 
+from cktk.core.types import EdgeList, GraphType
 from cktk.utils.df_utils import (
     edges_from_columns,
     explode_on_columns,
 )
-from cktk.utils.graph_util import GraphType, get_graph_type
+from cktk.utils.graph_util import get_graph_type
+
+logger = logging.getLogger(__name__)
+
+
+class TransformerAttributeError(AttributeError):
+    def __init__(self, value: str):
+        msg = f'{value} must be defined'
+        super().__init__(msg)
 
 
 @dataclass(eq=True, frozen=True)
@@ -27,7 +37,7 @@ class NetworkGraphTransformer:
         Args:
             df: DataFrameType object
         """
-        self.edges: list[tuple[str, str]]
+        self.edges: EdgeList
         self.nodes: list[str]
         self.explode_columns: list[str] | None
         self.explode_delimitter: str
@@ -38,15 +48,11 @@ class NetworkGraphTransformer:
         self.edge_label_attr: str
         self.directed: bool
         self.multi: bool
-
-    def apply_explode(self) -> pl.DataFrame:
-        return explode_on_columns(self)
+        self.df: pl.DataFrame
 
     def create_graph(self) -> GraphType:
         """Creates networkx graph from a dataframe."""
-        if self.explode_columns is not None:
-            working_df = self.apply_explode()
-        else:
-            working_df = self.df
+        working_df = explode_on_columns(self)
         g = get_graph_type(self)
+        print(g)
         edges_from_columns(working_df, self.edges, self.edge_attrs)
--- src/cktk/utils/df_utils.py
+++ src/cktk/utils/df_utils.py
@@ -2,10 +2,16 @@ import logging
 
 import polars as pl
 
-from cktk.core.types import EdgeList
 from cktk.core.network_graph_transformer import NetworkGraphTransformer
+from cktk.core.types import EdgeList
 
 logger = logging.getLogger(__name__)
+
+
+class DataFrameValueError(ValueError):
+    def __init__(self, value: str):
+        msg = f'{value} must be defined'
+        super().__init__(msg)
 
 
 def explode_on_columns(obj: NetworkGraphTransformer) -> pl.DataFrame:
@@ -22,4 +28,31 @@ def explode_on_columns(obj: NetworkGraphTransformer) -> pl.DataFrame:
 
 def edges_from_columns(
     df: pl.DataFrame, edges: EdgeList, edge_attrs: list[str]
-) -> pl.DataFrame: ...
+) -> pl.DataFrame:
+    working_df = df
+    if not edges:
+        raise DataFrameValueError('edges')
+
+    edge_dfs: list[pl.DataFrame] = []
+
+    for source_col, target_col in edges:
+        if (
+            source_col not in working_df.columns
+            or target_col not in working_df.columns
+        ):
+            raise ValueError(
+                f'Missing expected column: {source_col} or {target_col}'
+            )
+
+        selected_cols = [
+            pl.col(source_col).alias('source'),
+            pl.col(target_col).alias('target'),
+        ]
+        for attr in self.config.edge_attrs:
+            if attr in self.df.columns:
+                selected_cols.append(pl.col(attr))
+
+        temp_edges = self.df.select(selected_cols).with_columns([
+            pl.lit(target_col).alias('type')
+        ])
+        edge_dfs.append(temp_edges)
